7765917ce1c9764ce0764d949d3d70ba2ecbe4c4
حمد المري <3954563+hamadmarri@users.noreply.github.com>
2023-11-15 11:24:45 +0300

    Clean up

--- lib/bench/csp_bench.ex
+++ lib/bench/csp_bench.ex
@@ -2,10 +2,16 @@ defmodule Bench.CspBench do
   def run do
     Benchee.run(
       %{
-        "list1" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 10]) end,
-        "list2" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 10]) end,
-        "list3" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 10]) end,
-        "list4" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 10]) end,
+        "list1" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 5, 10]) end,
+        "list2" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 5, 10]) end,
+        "list3" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 5, 10]) end,
+        "list4" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 5, 10]) end,
+        "list5" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 5, 10]) end,
+        "list6" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 5, 10]) end,
+        "list7" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 5, 10]) end,
+        "list8" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 5, 10]) end,
+        "list9" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 5, 10]) end,
+        "list10" => fn -> Csp.NormalList.start([7, 7, 7, 7, 7, 7, 7, 5, 10]) end
       },
       # warmup: 0,
       time: 5
--- lib/csp_normal_list/csp_children_nl.ex
+++ lib/csp_normal_list/csp_children_nl.ex
@@ -7,7 +7,7 @@ defmodule Csp.NormalList.Children do
     # IO.puts("q INIT CHILDREN")
 
     # in the form [1], [2], ..
-    Enum.to_list(1..@entropy) |> Enum.map(&[&1]) |> Enum.reverse()
+    Enum.to_list(@entropy..1//-1) |> Enum.map(&[&1])
   end
 
   def generate(q, parent_items, stat)
@@ -18,7 +18,7 @@ defmodule Csp.NormalList.Children do
 
   def generate(q, parent_items, stat) do
     # IO.puts("GEN CHILDREN for #{inspect(parent_items)}")
-    children = for c <- @entropy..1, do: [c | parent_items]
+    children = for c <- @entropy..1//-1, do: [c | parent_items]
 
     # add children to stack
     {children ++ q, stat + @entropy}
